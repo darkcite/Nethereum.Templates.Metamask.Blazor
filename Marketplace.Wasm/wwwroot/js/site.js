59399baaa67f0da4869b62a4dd292ade41fcfd81
darkcite <qqq.darkcite@gmail.com>
2023-05-11 02:28:15 -0500

    ++

--- Marketplace.Wasm/wwwroot/js/site.js
+++ Marketplace.Wasm/wwwroot/js/site.js
@@ -1,15 +1,12 @@
-﻿window.openModal = function (modalId) {
-    let modal = new bootstrap.Modal(document.getElementById(modalId));
+﻿window.openModal = (modalId) => {
+    var modal = new bootstrap.Modal(document.getElementById(modalId));
     modal.show();
-}
+};
 
-window.closeModal = function (modalId) {
-    let modal = bootstrap.Modal.getInstance(document.getElementById(modalId));
-    if (modal) {
-        modal.hide();
-    }
-}
-
+window.closeModal = (modalId) => {
+    var modal = new bootstrap.Modal(document.getElementById(modalId));
+    modal.hide();
+};
 function openModal(modalId) {
     $(`#${modalId}`).modal('show');
 }
