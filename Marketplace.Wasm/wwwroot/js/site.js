f20a6ad2394b2e42e6861aab2e0f0b010d08e2d2
darkcite <qqq.darkcite@gmail.com>
2023-05-11 02:13:44 -0500

    +  update button

---
+++ Marketplace.Wasm/wwwroot/js/site.js
@@ -0,0 +1,19 @@
+﻿window.openModal = function (modalId) {
+    let modal = new bootstrap.Modal(document.getElementById(modalId));
+    modal.show();
+}
+
+window.closeModal = function (modalId) {
+    let modal = bootstrap.Modal.getInstance(document.getElementById(modalId));
+    if (modal) {
+        modal.hide();
+    }
+}
+
+function openModal(modalId) {
+    $(`#${modalId}`).modal('show');
+}
+
+function closeModal(modalId) {
+    $(`#${modalId}`).modal('hide');
+}
