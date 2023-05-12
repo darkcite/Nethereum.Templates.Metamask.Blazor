0a50b8b94ab79d7edd2d884c6ddf7c0ed375aa34
darkcite <qqq.darkcite@gmail.com>
2023-05-12 17:36:41 -0500

    NFT Service refactored

--- Marketplace.Wasm/Services/NFTService.cs
+++ Marketplace.Wasm/Services/NFTService.cs
@@ -18,15 +18,13 @@ namespace Marketplace.Wasm.Services
 {
     public class NFTService
     {
-        private EthereumClientService _ethereumClientService;
+        private readonly EthereumClientService _ethereumClientService;
         private readonly IConfiguration _configuration;
-        private MetaMaskService _metaMaskService;
-
+        private readonly MetaMaskService _metaMaskService;
         private readonly string _contractAddress;
         private readonly HexBigInteger _deploymentBlockNumber;
         private readonly Web3 _web3;
-
-        private MyERC1155Service _erc1155Service;
+        private readonly MyERC1155Service _erc1155Service;
 
         public NFTService(EthereumClientService ethereumClientService, IConfiguration configuration, MetaMaskService metaMaskService)
         {
@@ -46,61 +44,63 @@ namespace Marketplace.Wasm.Services
         private async Task<List<NFT>> LoadNFTs(Func<TokenDataOutputDTO, bool> filter, string account = null, bool checkOwnership = false)
         {
             List<NFT> NFTs = new List<NFT>();
-
-            var web3 = _ethereumClientService.GetWeb3();
-            web3.Eth.TransactionManager.UseLegacyAsDefault = true;
-
             var filterInput = _erc1155Service.GetTokenMintedEvent().CreateFilterInput(
                 new BlockParameter(_deploymentBlockNumber),
                 BlockParameter.CreateLatest());
-            var eventLogs = await web3.Eth.Filters.GetLogs.SendRequestAsync(filterInput);
+            var eventLogs = await _web3.Eth.Filters.GetLogs.SendRequestAsync(filterInput).ConfigureAwait(false);
 
             var decodedLog = _erc1155Service.GetTokenMintedEvent().DecodeAllEventsForEvent(eventLogs);
             foreach (var log in decodedLog)
             {
-                string tokenUri = await _erc1155Service.UriQueryAsync(log.Event.TokenId);
-                var ipfsGatewayUrl = _configuration.GetValue<string>("IPFS:GatewayUrl");
-                string httpGatewayUrl = tokenUri.Replace("ipfs://", ipfsGatewayUrl);
-
-                using HttpClient httpClient = new HttpClient();
-                HttpResponseMessage response = await httpClient.GetAsync(httpGatewayUrl);
-
-                if (response.IsSuccessStatusCode)
+                var (tokenData, metadata) = await GetTokenData(log.Event.TokenId).ConfigureAwait(false);
+                if (tokenData != null && metadata != null && filter(tokenData) && (!checkOwnership || await IsTokenOwnedByAccount(account, log.Event.TokenId)))
                 {
-                    string metadataJson = await response.Content.ReadAsStringAsync();
-                    NFTMetadata metadata = JsonConvert.DeserializeObject<NFTMetadata>(metadataJson);
-
-                    var tokenData = await _erc1155Service.GetTokenDataAsync(log.Event.TokenId);
-
-                    // Check if the current owner of the token is the provided account
-                    if (checkOwnership)
+                    NFTs.Add(new NFT
                     {
-                        string currentOwner = await _erc1155Service.GetOwnerOfTokenAsync(metadata.ProductId);
-                        if (account != null && currentOwner != account)
-                        {
-                            // Skip this token if the current owner is not the provided account
-                            continue;
-                        }
-                    }
-
-                    if (filter(tokenData))
-                    {
-                        NFTs.Add(new NFT
-                        {
-                            Image = metadata.Image,
-                            Description = metadata.Description,
-                            Name = metadata.Name,
-                            TokenId = metadata.ProductId,
-                            Price = tokenData.Price,
-                            ForSale = tokenData.ForSale
-                        });
-                    }
+                        Image = metadata.Image,
+                        Description = metadata.Description,
+                        Name = metadata.Name,
+                        TokenId = metadata.ProductId,
+                        Price = tokenData.Price,
+                        ForSale = tokenData.ForSale
+                    });
                 }
             }
 
             return NFTs;
         }
 
+        private async Task<(TokenDataOutputDTO, NFTMetadata)> GetTokenData(BigInteger tokenId)
+        {
+            string tokenUri = await _erc1155Service.UriQueryAsync(tokenId).ConfigureAwait(false);
+            var metadata = await GetMetadataFromUri(tokenUri).ConfigureAwait(false);
+            var tokenData = await _erc1155Service.GetTokenDataAsync(tokenId).ConfigureAwait(false);
+            return (tokenData, metadata);
+        }
+
+        private async Task<NFTMetadata> GetMetadataFromUri(string tokenUri)
+        {
+            var ipfsGatewayUrl = _configuration.GetValue<string>("IPFS:GatewayUrl");
+            string httpGatewayUrl = tokenUri.Replace("ipfs://", ipfsGatewayUrl);
+
+            using HttpClient httpClient = new HttpClient();
+            HttpResponseMessage response = await httpClient.GetAsync(httpGatewayUrl).ConfigureAwait(false);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                return null;
+            }
+
+            string metadataJson = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+            return JsonConvert.DeserializeObject<NFTMetadata>(metadataJson);
+        }
+
+        // Сheck if the current owner of the token is the provided account
+        private async Task<bool> IsTokenOwnedByAccount(string account, BigInteger tokenId)
+        {
+            string currentOwner = await _erc1155Service.GetOwnerOfTokenAsync(tokenId).ConfigureAwait(false);
+            return account != null && currentOwner == account;
+        }
 
         public Task<List<NFT>> LoadAllNFTs(string account = null) => LoadNFTs(_ => true, account, false);
 
@@ -108,36 +108,27 @@ namespace Marketplace.Wasm.Services
 
         public Task<List<NFT>> LoadNFTsOwnedByAccount(string account) => LoadNFTs(_ => true, account, true);
 
-
         public async Task UpdateNFTDetailsAsync(BigInteger tokenId, BigInteger newPrice, bool newStatus, string newContactInfo)
         {
-            // Get the function for SetApprovalForAll
             var setApprovalFunction = _erc1155Service.ContractHandler.GetFunction<SetApprovalForAllFunction>();
 
-            // Prepare the function call
             var approvalFunctionMessage = new SetApprovalForAllFunction
             {
                 Operator = _contractAddress,
                 Approved = true
             };
 
-            // Encode the function call
             var approvalData = setApprovalFunction.GetData(approvalFunctionMessage);
 
-            // Send the transaction through Metamask
             var approvalTxHash = await _metaMaskService.SendTransaction(_contractAddress, approvalData);
 
-            // Wait for the transaction to be mined and get the receipt
             var receipt = await _web3.Eth.Transactions.GetTransactionReceipt.SendRequestAsync(approvalTxHash);
 
-            // Check the receipt status
             if (receipt.Status.Value == 0)
             {
-                // if the transaction was not successful, throw an exception
                 throw new Exception("Failed to approve transaction");
             }
 
-            // Prepare function call for updating token
             var updateFunctionMessage = new UpdateTokenForSaleFunction
             {
                 Id = tokenId,
@@ -145,23 +136,16 @@ namespace Marketplace.Wasm.Services
                 NewStatus = newStatus,
                 NewContactInfo = newContactInfo
             };
-
-            // Get function from service for updating token
             var updateFunction = _erc1155Service.ContractHandler.GetFunction<UpdateTokenForSaleFunction>();
 
-            // Encode the function call for updating token
             var updateData = updateFunction.GetData(updateFunctionMessage);
 
-            // Send the transaction through Metamask
             var updateTxHash = await _metaMaskService.SendTransaction(_contractAddress, updateData);
 
-            // Wait for the transaction to be mined and get the receipt
             var receipt2 = await _web3.Eth.Transactions.GetTransactionReceipt.SendRequestAsync(approvalTxHash);
 
-            // Check the receipt status
             if (receipt2.Status.Value == 0)
             {
-                // if the transaction was not successful, throw an exception
                 throw new Exception("Failed to update token");
             }
         }
@@ -170,7 +154,7 @@ namespace Marketplace.Wasm.Services
         {
             // Get the function for BuyToken
             var buyTokenFunction = _erc1155Service.ContractHandler.GetFunction<BuyTokenFunction>();
-            
+
             // Prepare the function call
             var buyTokenFunctionMessage = new BuyTokenFunction
             {
@@ -187,46 +171,28 @@ namespace Marketplace.Wasm.Services
             // Wait for the transaction to be mined and get the receipt
             var receipt = await _web3.Eth.Transactions.GetTransactionReceipt.SendRequestAsync(buyTokenTxHash);
 
-            // Check the receipt status
             if (receipt.Status.Value == 0)
             {
-                // if the transaction was not successful, throw an exception
                 throw new Exception("Failed to buy token");
             }
         }
 
         public async Task<List<NFT>> GetAllTokensOwnedByAccountAsync(string account)
         {
-
-            List<NFT> NFTs = new List<NFT>();
-
-            var web3 = _ethereumClientService.GetWeb3();
-            web3.Eth.TransactionManager.UseLegacyAsDefault = true;
-
+            var NFTs = new List<NFT>();
             var filterInput = _erc1155Service.GetTokenMintedEvent().CreateFilterInput(
                 new BlockParameter(_deploymentBlockNumber),
                 BlockParameter.CreateLatest());
-            var eventLogs = await web3.Eth.Filters.GetLogs.SendRequestAsync(filterInput);
+            var eventLogs = await _web3.Eth.Filters.GetLogs.SendRequestAsync(filterInput).ConfigureAwait(false);
 
             var decodedLog = _erc1155Service.GetTokenMintedEvent().DecodeAllEventsForEvent(eventLogs);
 
             foreach (var log in decodedLog)
             {
-                string tokenUri = await _erc1155Service.UriQueryAsync(log.Event.TokenId);
-                var ipfsGatewayUrl = _configuration.GetValue<string>("IPFS:GatewayUrl");
-                string httpGatewayUrl = tokenUri.Replace("ipfs://", ipfsGatewayUrl);
-
-                using HttpClient httpClient = new HttpClient();
-                HttpResponseMessage response = await httpClient.GetAsync(httpGatewayUrl);
-
-                if (response.IsSuccessStatusCode)
+                var (tokenData, metadata) = await GetTokenData(log.Event.TokenId).ConfigureAwait(false);
+                if (tokenData != null && metadata != null)
                 {
-                    string metadataJson = await response.Content.ReadAsStringAsync();
-                    NFTMetadata metadata = JsonConvert.DeserializeObject<NFTMetadata>(metadataJson);
-
-                    var tokenData = await _erc1155Service.GetTokenDataAsync(log.Event.TokenId);
-
-                    var balance = await _erc1155Service.BalanceOfQueryAsync(account, log.Event.TokenId);
+                    var balance = await _erc1155Service.BalanceOfQueryAsync(account, log.Event.TokenId).ConfigureAwait(false);
                     if (balance > 0)
                     {
                         NFTs.Add(new NFT
@@ -238,10 +204,11 @@ namespace Marketplace.Wasm.Services
                             Price = tokenData.Price,
                             ForSale = tokenData.ForSale
                         });
-                    }             
+                    }
                 }
             }
             return NFTs;
         }
     }
 }
+
